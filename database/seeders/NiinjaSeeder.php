a97ab660e9c3b1305a99573b3ae826c06953dbb8
Hamza El-Alamy <hamza.elalamy1@uit.ac.ma>
2025-03-12 13:58:19 +0000

    Add NiinjaSeeder to populate database with 50 Niinja records

--- database/seeders/NiinjaSeeder.php
+++ database/seeders/NiinjaSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Niinja;
 
 class NiinjaSeeder extends Seeder
 {
@@ -12,6 +13,6 @@ class NiinjaSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Niinja::factory->count(50)->create();
     }
 }
